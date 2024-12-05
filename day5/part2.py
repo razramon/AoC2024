2f33509202b0601f211239ca62d2ef02704df433
Raz Ramon <razramon123@gmail.com>
2024-12-05 17:56:43 +0200

    day5 solutions

---
+++ day5/part2.py
@@ -0,0 +1,73 @@
+from pathlib import Path
+import time
+import re
+from collections import defaultdict
+
+SCRIPT_DIR = Path(__file__).parent
+INPUT_FILE = Path(SCRIPT_DIR, "input.txt")
+
+
+def reorder(update, before, after):
+    for i in range(len(update)):
+        if update[i] not in before or update[i] not in after:
+            continue
+        for j in range(i):
+            if update[j] not in before[update[j]]:
+                continue
+            if update[j] not in before[update[i]]:
+                update[i], update[j] = update[j], update[i]
+        for j in range(i + 1, len(update)):
+            if update[j] not in after:
+                continue
+            if update[j] not in after[update[i]]:
+                update[i], update[j] = update[j], update[i]
+    return update
+
+
+def check_order(update, before, after):
+    for i in range(len(update)):
+        if update[i] not in before or update[i] not in after:
+            continue
+        for j in range(i):
+            if update[j] not in before[update[j]]:
+                continue
+            if update[j] not in before[update[i]]:
+                return False
+        for j in range(i + 1, len(update)):
+            if update[j] not in after:
+                continue
+            if update[j] not in after[update[i]]:
+                return False
+    return True
+
+
+def question(updates, before, after):
+    ans = 0
+    for update in updates:
+        update = update.split(",")
+        if not check_order(update, before, after):
+            update = reorder(update, before, after)
+            ans += int(update[len(update) // 2])
+
+    return ans
+
+
+def main():
+    with open(INPUT_FILE, mode="rt") as f:
+        content = f.read().splitlines()
+    before_order = defaultdict(list)
+    after_order = defaultdict(list)
+    for i, line in enumerate(content):
+        if line == "":
+            break
+        line = line.split("|")
+        before_order[line[1]].append(line[0])
+        after_order[line[0]].append(line[1])
+    print(question(content[i + 1 :], before_order, after_order))
+
+
+if __name__ == "__main__":
+    t1 = time.perf_counter()
+    main()
+    t2 = time.perf_counter()
+    print(f"Execution time: {t2 - t1:0.4f} seconds")
