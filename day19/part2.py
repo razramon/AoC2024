22730110cdb9b2162eb9979587e55c5d77823352
Raz Ramon <razramon123@gmail.com>
2024-12-19 12:19:20 +0200

    part2 solution

---
+++ day19/part2.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+import time
+
+SCRIPT_DIR = Path(__file__).parent
+INPUT_FILE = Path(SCRIPT_DIR, "input.txt")
+
+
+# dynamic programming
+def check_possible(p, comb):
+    n = len(p)
+    dp = [0] * n
+    for i in range(n):
+        if p[: i + 1] in comb:
+            dp[i] += 1
+
+        for c in comb:
+            if p[i - len(c) + 1 : i + 1] == c and dp[i - len(c)] > 0:
+                dp[i] += dp[i - len(c)]
+    return dp[-1]
+
+
+def question(comb, poss):
+    ans = 0
+    for p in poss:
+        ans += check_possible(p, comb)
+    print(ans)
+
+
+def main():
+    with open(INPUT_FILE, mode="rt") as f:
+        content = f.read().splitlines()
+    comb = content[0].split(", ")
+    question(comb, content[2:])
+
+
+if __name__ == "__main__":
+    t1 = time.perf_counter()
+    main()
+    t2 = time.perf_counter()
+    print(f"Execution time: {t2 - t1:0.4f} seconds")
